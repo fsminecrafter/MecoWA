6779805e256b73b8def52faad1110fd808ccbece
joelw <joelw@JOEL_MINECRAFT>
2025-11-07 18:24:10 +0100

    Add project files.

---
+++ Core/Scripts/mainscript/objloader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+
+struct OBJData {
+    std::vector<float> vertexCoords;   // x, y, z
+    std::vector<float> vertexNormals;  // nx, ny, nz
+    std::vector<float> vertexColors;   // r, g, b
+    std::vector<unsigned int> elementArray; // indices
+	std::vector<float> vertexUVs; // vertex uv stuff
+
+};
+
+
+bool loadOBJ(const std::string& path, OBJData& out);
